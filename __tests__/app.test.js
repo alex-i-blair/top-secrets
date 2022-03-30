7fbfbd0b324f7db4938c42ed97a499209971d48d
Alex Blair <bclan11@gmail.com>
2022-03-30 13:01:09 -0700

    sign in existing user route passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
 describe('top-secrets routes', () => {
   beforeEach(() => {
@@ -11,6 +12,7 @@ describe('top-secrets routes', () => {
   afterAll(() => {
     pool.end();
   });
+
   it('should be able to create/sign up a user', async () => {
     const res = await request(app)
       .post('/api/v1/auth/signup')
@@ -18,6 +20,21 @@ describe('top-secrets routes', () => {
     expect(res.body).toEqual({
       id: expect.any(String),
       username: 'Toby',
+    });
+  });
+
+  it('should sign in an existing user', async () => {
+    const user = await UserService.create({
+      username: 'Toby',
+      password: 'spoon',
+    });
+    const res = await request(app)
+      .post('/api/v1/auth/signin')
+      .send({ username: 'Toby', password: 'spoon' });
+
+    expect(res.body).toEqual({
+      message: 'Sign in successful',
+      user,
     });
   });
 });
